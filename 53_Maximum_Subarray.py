3823c4abfda13ec8c903b5d7f959058e9cd939f1
Doyle <liuling_poppy@163.com>
2018-08-17 19:23:06 +0800

    17-Aug

---
+++ 53_Maximum_Subarray.py
@@ -0,0 +1,23 @@
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+# Example:
+
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = maxsum = nums[0]
+        for i in nums:
+        	count = max(i,i+count)
+        	maxsum = max(maxsum,count)
+        return maxsum
+s = Solution()
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(s.maxSubArray(nums))
+
+        
